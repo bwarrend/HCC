f03f5bc0da4f0bb75681ff020863b8d80a34ca8e
Brett W <bwarrend@my.hpu.edu>
2020-02-08 22:21:52 -1000

    tweaks and preparing for text/image from url

---
+++ Assets/Scripts/SetTextFromURL.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetTextFromURL : MonoBehaviour{
+
+    public string url;
+    public Text text;
+
+    IEnumerator Start()
+    {
+        WWW www = new WWW(url);
+        yield return www;
+        text.text = www.text;
+    }
+}
