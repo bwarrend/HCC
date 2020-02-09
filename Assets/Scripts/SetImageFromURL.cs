7275eec5436143059057009b942f01e7d870fa0e
Brett W <bwarrend@my.hpu.edu>
2020-02-08 20:43:21 -1000

    migrating to URL image system

---
+++ Assets/Scripts/SetImageFromURL.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetImageFromURL : MonoBehaviour{
+
+	public Image img;
+	public string url;
+	IEnumerator Start()
+	{
+		WWW www = new WWW(url);
+		//UnityWebRequest www = UnityWebRequest.Get(url);
+		yield return www;
+		img.sprite = Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), new Vector2(0, 0));
+	}
+}
