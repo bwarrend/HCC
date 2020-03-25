334dd885457d0b9e49a86b070cf1e02b347469f9
byuen1 <bensonjyuen@gmail.com>
2020-03-24 20:00:01 -1000

    email tabs delete

---
+++ Assets/Scripts/EmailManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EmailManager : MonoBehaviour
+{
+
+    public void DeleteObject(GameObject item)
+    {
+        Destroy(item.gameObject);
+    }
+
+    //public void DeleteEmail(EmailObject email)
+    //{
+    //    emailObjects.Remove(email);
+    //    Destroy(email.gameObject);
+    //}
+}
