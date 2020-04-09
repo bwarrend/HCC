14abf5f0a016c69e730a5461028d62bf8a9e77b7
byuen1 <bensonjyuen@gmail.com>
2020-04-09 11:07:41 -1000

    created start scene to play to login screen. Linked the email icon to the updated email scene

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,25 +7,28 @@ public class TestScript : MonoBehaviour
     public Dialogue dialogue;
     private bool clicked;
 
-    public void start(){
+    void Start()
+    {
         clicked = false;
+        TriggerDialogue();
     }
     public void TriggerDialogue()
-    {   
+    {
         clicked = true;
         DialogueManager.instance.EnqueueDialogue(dialogue);
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            if(!clicked){
-            TriggerDialogue();
-            }
+    //private void Update()
+    //{
+    //    if (Input.GetKeyUp(KeyCode.Space))
+    //    {
+    //        if (!clicked)
+    //        {
+                
+    //        }
 
-        }
+    //    }
 
-    }
+    //}
 
 }
