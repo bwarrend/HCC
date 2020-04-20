6da17c97f518764719238e9802947d277b945f82
Brett W <bwarrend@my.hpu.edu>
2020-04-19 16:14:35 -1000

    Progress bar fills based on playerprefs progress system

--- Assets/Scripts/Set_Player_Progress.cs
+++ Assets/Scripts/Set_Player_Progress.cs
@@ -5,7 +5,25 @@ using UnityEngine.UI;
 
 public class Set_Player_Progress : MonoBehaviour
 {
-    public ProgressBar prog;
+    //public ProgressBar prog;
+
+
+    public static float getPlayerProgress(){
+        float percentage = 0f;
+
+        if(PlayerPrefs.GetInt("InternetTab1") == 1) percentage += 10f;
+        if(PlayerPrefs.GetInt("InternetTab2") == 1) percentage += 10f;
+        if(PlayerPrefs.GetInt("InternetTab3") == 1) percentage += 10f;
+        if(PlayerPrefs.GetInt("InternetTab4") == 1) percentage += 10f;
+        if(PlayerPrefs.GetInt("InternetTab5") == 1) percentage += 10f;
+        if(PlayerPrefs.GetInt("EmailTab1") == 1) percentage += 10f;
+        if(PlayerPrefs.GetInt("EmailTab2") == 1) percentage += 10f;
+        if(PlayerPrefs.GetInt("EmailTab3") == 1) percentage += 10f;
+        if(PlayerPrefs.GetInt("EmailTab4") == 1) percentage += 10f;
+        if(PlayerPrefs.GetInt("EmailTab5") == 1) percentage += 10f;
+
+        return percentage;
+    }
 
     public void clearPlayerProgress()
     {
@@ -24,49 +42,49 @@ public class Set_Player_Progress : MonoBehaviour
 
     public void progressedInternetTab1(){
         PlayerPrefs.SetInt("InternetTab1", 1);
-        prog.IncrementProgress(1);
+        //prog.IncrementProgress(1);
     }
 
     public void progressedInternetTab2(){
         PlayerPrefs.SetInt("InternetTab2", 1);
-        prog.IncrementProgress(1);
+        //prog.IncrementProgress(1);
     }
 
     public void progressedInternetTab3(){
         PlayerPrefs.SetInt("InternetTab3", 1);
-        prog.IncrementProgress(1);
+        //prog.IncrementProgress(1);
     }
 
     public void progressedInternetTab4(){
         PlayerPrefs.SetInt("InternetTab4", 1);
-        prog.IncrementProgress(1);
+        //prog.IncrementProgress(1);
     }
 
     public void progressedInternetTab5(){
         PlayerPrefs.SetInt("InternetTab5", 1);
-        prog.IncrementProgress(1);
+        //prog.IncrementProgress(1);
     }
 
 
     public void progressedEmailTab1(){
         PlayerPrefs.SetInt("EmailTab1", 1);
-        prog.IncrementProgress(1);
+        //prog.IncrementProgress(1);
     }
     public void progressedEmailTab2(){
         PlayerPrefs.SetInt("EmailTab2", 1);
-        prog.IncrementProgress(1);
+        //prog.IncrementProgress(1);
     }
     public void progressedEmailTab3(){
         PlayerPrefs.SetInt("EmailTab3", 1);
-        prog.IncrementProgress(1);
+        //prog.IncrementProgress(1);
     }
     public void progressedEmailTab4(){
         PlayerPrefs.SetInt("EmailTab4", 1);
-        prog.IncrementProgress(1);
+        //prog.IncrementProgress(1);
     }
     public void progressedEmailTab5(){
         PlayerPrefs.SetInt("EmailTab5", 1);
-        prog.IncrementProgress(1);
+        //prog.IncrementProgress(1);
     }
 
     
